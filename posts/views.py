c86c21a666e1903756d6250c9a8e9774d3c2b9ce
Chaz Selph <chaz@askkanzi.com>
2022-04-03 16:40:25 -0400

    posts app functional

---
+++ posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+	posts = Post.objects.all()
+
+	return render(request, 'posts/index.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+	post = Post.objects.get(slug=slug)
+
+	return render(request, 'posts/post_detail.html', {'post': post})
